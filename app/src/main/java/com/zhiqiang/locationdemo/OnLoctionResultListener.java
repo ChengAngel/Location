dda0615631271b0d13c619c77d558b09e4719e4c
chengzhiqiang <cheng.zhiqiang@boshtc.com>
2017-08-05 14:42:27 +0800

    优化文件，将定位代码写成一个工具类。通过回调返回定位结果

---
+++ app/src/main/java/com/zhiqiang/locationdemo/OnLoctionResultListener.java
@@ -0,0 +1,15 @@
+package com.zhiqiang.locationdemo;
+
+/**
+ * ================================================
+ * 作    者：程志强
+ * 邮    箱：18394188838@163.com
+ * 版    本：1.0.0
+ * 创建日期：2017/08/05  14:29
+ * 描    述：定位结果的回调
+ * 修订历史：
+ * ================================================
+ */
+public interface OnLoctionResultListener {
+    void onLoctionResultListener(String locationInfo);
+}
